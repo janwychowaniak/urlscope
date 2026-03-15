a75de2ac9418f24e19ef066e0d5c4b61932b4773
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2026-03-15 18:50:32 +0100

    T007: Update __init__.py to include all model exports

--- src/urlscope/models/__init__.py
+++ src/urlscope/models/__init__.py
@@ -1,1 +1,21 @@
+from ._result import (
+    BrandMatch,
+    CertificateInfo,
+    PageInfo,
+    ScanLists,
+    ScanResult,
+    TaskInfo,
+    Verdicts,
+)
+from ._submission import SubmissionResponse
 
+__all__ = [
+    "SubmissionResponse",
+    "ScanResult",
+    "TaskInfo",
+    "PageInfo",
+    "Verdicts",
+    "BrandMatch",
+    "ScanLists",
+    "CertificateInfo",
+]
